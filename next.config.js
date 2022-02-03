fdf50fc291687effd4fc4f6d348a38b3f7876688
Jack Oddy <jack.oddy91@gmail.com>
2022-02-03 10:23:58 +0000

    with beforeFiles rewrites in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
+    rewrites: async () => ({
+        beforeFiles: [
+            {
+                source: '/overwritten',
+                destination: 'https://pokeapi.co/api/v2/pokemon/ditto'
+            }
+        ]
+    }),
   reactStrictMode: true,
 }
